284df325fcc295bbf30ed480ea461c11bfb7c0f3
Boolean Nguyen <bvn006@ucsd.edu>
2018-01-24 02:36:30 -0800

    bfs and ucs start

--- methods.py
+++ methods.py
@@ -53,36 +53,36 @@ class Agent:
         elif self.type == "astar":
             self.astar_step()
     def dfs_step(self):
-        #...
+        #... see if it ran out of options
         if not self.frontier:
             self.failed = True
             print("no path")
             return
         current = self.frontier.pop()
         print("popped: ", current)
-        #...
+        #... keep track
         self.grid.nodes[current[0]][current[1]].checked = True
         self.grid.nodes[current[0]][current[1]].frontier = False
         self.checked.append(current)
-        #...
+        #... loop through possible next step
         for i, j in self.actions:
-            #...
+            #... tests the 4 action relative to current
             nextstep = (current[0]+i, current[1]+j)
-            #...
+            #... prevent from visiting again
             #See what happens if you disable this check here
             if nextstep in self.checked or nextstep in self.frontier:
                 print("expanded before: ", nextstep)
                 continue
-            #...
+            #... check if it's in boundaries of the grid
             if 0 <= nextstep[0] < self.grid.row_range:
                 if 0 <= nextstep[1] < self.grid.col_range:
-                    #...
+                    #... check if puddle
                     if not self.grid.nodes[nextstep[0]][nextstep[1]].puddle:
                         if nextstep == self.goal:
                             self.finished = True
-                        #...
+                        #... add to the frontier which will be traversed to eventually
                         self.frontier.append(nextstep)
-                        #...
+                        #... 
                         self.grid.nodes[nextstep[0]][nextstep[1]].frontier = True
                         #...
                         self.came_from[nextstep] = current
@@ -94,35 +94,36 @@ class Agent:
             else:
                 print("out of row range: ", nextstep)
     def bfs_step(self):
-                #...
+        #... see if it ran out of options
         if not self.frontier:
-            self.failed = True
+            self.finished = True
             print("no path")
             return
-        current = self.frontier.pop()
-        print("popped: ", current)
-        #...
+        current = heappop(self.frontier)[1]
+        cost = self.gdict[current]
+        print("popped: ", current, "cost: ", cost)
+        #... keep track
         self.grid.nodes[current[0]][current[1]].checked = True
         self.grid.nodes[current[0]][current[1]].frontier = False
         self.checked.append(current)
-        #...
+        #... loop through possible next step
         for i, j in self.actions:
-            #...
+            #... tests the 4 action relative to current
             nextstep = (current[0]+i, current[1]+j)
-            #...
+            #... prevent from visiting again
             #See what happens if you disable this check here
             if nextstep in self.checked or nextstep in self.frontier:
                 print("expanded before: ", nextstep)
                 continue
-            #...
+            #... check if it's in boundaries of the grid
             if 0 <= nextstep[0] < self.grid.row_range:
                 if 0 <= nextstep[1] < self.grid.col_range:
-                    #...
+                    #... check if puddle
                     if not self.grid.nodes[nextstep[0]][nextstep[1]].puddle:
                         if nextstep == self.goal:
                             self.finished = True
                         #...
-                        self.frontier.append(nextstep)
+                        self.frontier.insert(0, nextstep)
                         #...
                         self.grid.nodes[nextstep[0]][nextstep[1]].frontier = True
                         #...
@@ -136,7 +137,46 @@ class Agent:
                 print("out of row range: ", nextstep)
     def ucs_step(self):
         #[Hint] you can get the cost of a node by node.cost()
-        pass
+        #... see if it ran out of options
+        if not self.frontier:
+            self.finished = True
+            print("no path")
+            return
+        current = self.frontier.pop()
+        print("popped: ", current)
+        #... keep track
+        self.grid.nodes[current[0]][current[1]].checked = True
+        self.grid.nodes[current[0]][current[1]].frontier = False
+        self.checked.append(current)
+        #... loop through possible next step
+        for i, j in self.actions:
+            #... tests the 4 action relative to current
+            nextstep = (current[0]+i, current[1]+j)
+            #... prevent from visiting again
+            #See what happens if you disable this check here
+            if nextstep in self.checked or nextstep in self.frontier:
+                print("expanded before: ", nextstep)
+                continue
+            #... check if it's in boundaries of the grid
+            if 0 <= nextstep[0] < self.grid.row_range:
+                if 0 <= nextstep[1] < self.grid.col_range:
+                    #... check if puddle
+                    if not self.grid.nodes[nextstep[0]][nextstep[1]].puddle:
+                        if nextstep == self.goal:
+                            self.finished = True
+                        #...
+                        self.frontier.insert(0, nextstep)
+                        #...
+                        self.grid.nodes[nextstep[0]][nextstep[1]].frontier = True
+                        #...
+                        self.came_from[nextstep] = current
+                        print("pushed: ", nextstep)
+                    else:
+                        print("puddle at: ", nextstep)
+                else:
+                    print("out of column range: ", nextstep)
+            else:
+                print("out of row range: ", nextstep)
     #[Hint] you need to declare a heuristic function for Astar
     def astar_step(self):
         pass
